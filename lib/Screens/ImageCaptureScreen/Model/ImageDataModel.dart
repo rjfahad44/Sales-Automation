fd40a19b11db7115b12d08ef744912a9eeecb50d
Fahad <rjfahad44@gmail.com>
2024-11-16 14:43:18 +0600

    update..

--- lib/Screens/ImageCaptureScreen/Model/ImageDataModel.dart
+++ lib/Screens/ImageCaptureScreen/Model/ImageDataModel.dart
@@ -3,8 +3,7 @@ import 'package:hive/hive.dart';
 import '../../../global.dart';
 
 @HiveType(typeId: image_model_type_id)
-class ImageDataModel{
-
+class ImageDataModel {
   @HiveField(0)
   String imagePath;
   @HiveField(1)
@@ -12,11 +11,10 @@ class ImageDataModel{
   @HiveField(2)
   int doctorId;
 
-  ImageDataModel({
-    required this.imagePath,
-    required this.doctorName,
-    required this.doctorId
-  });
+  ImageDataModel(
+      {required this.imagePath,
+      required this.doctorName,
+      required this.doctorId});
 
   @override
   String toString() {
